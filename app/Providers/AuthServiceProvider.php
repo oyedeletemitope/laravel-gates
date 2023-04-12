9d4c1c9b3a266b1935bc8fd54361d59a2ee4f396
oyedeletemitope <meettopsy5fc@gmail.com>
2023-04-12 10:03:09 -0700

    first commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+ use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        Gate::define('subs-only', function($user) {
+            If ($user->subs ==1) {
+                return true;
+            }
+            return false;
+        });
+        
+    }
+}
